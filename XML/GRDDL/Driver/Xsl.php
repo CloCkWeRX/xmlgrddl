fcab4cb765357bebaf2094536c9ca0d66b52a6c1
daniel.oconnor <daniel.oconnor@74e7327c-5247-0410-af68-6f51a0f76bc8>
2008-03-01 02:42:24 +0000

    issue 4  * Boilerplate

--- XML/GRDDL/Driver/Xsl.php
+++ XML/GRDDL/Driver/Xsl.php
@@ -1,8 +1,54 @@
 <?php
+/**
+ * XML_GRDDL
+ *
+ * Copyright (c) 2008, Daniel O'Connor <daniel.oconnor@gmail.com>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Sebastian Bergmann nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Semantic_Web
+ * @package   XML_GRDDL
+ * @author    Daniel O'Connor <daniel.oconnor@gmail.com>
+ * @copyright 2008 Daniel O'Connor
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version   SVN: $Id: AssertionFailedError.php 1985 2007-12-26 18:11:55Z sb $
+ * @link      http://code.google.com/p/xmlgrddl/
+ */
+
 require_once 'XML/GRDDL/Driver.php';
 
-class XML_GRDDL_Driver_xsl extends XML_GRDDL_Driver {
-    public function __construct($options = array()) {
+class XML_GRDDL_Driver_Xsl extends XML_GRDDL_Driver
+{
+    public function __construct($options = array())
+    {
         if (!extension_loaded('xsl')) {
             throw new Exception("Don't forget to enable the xsl extension");
         }
@@ -10,7 +56,8 @@ class XML_GRDDL_Driver_xsl extends XML_GRDDL_Driver {
         parent::__construct($options);
     }
 
-    public function transform($stylesheet, $xml) {
+    public function transform($stylesheet, $xml)
+    {
         $dom = new DOMDocument('1.0');
         $dom->loadXML($xml);
 
