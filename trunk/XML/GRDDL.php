3578f91c8919f48b6bd4ee9c856587b8c4472e02
daniel.oconnor <daniel.oconnor@74e7327c-5247-0410-af68-6f51a0f76bc8>
2008-03-01 01:56:20 +0000

    issue 4  * Javadocs  * PHPCS friendly code

--- trunk/XML/GRDDL.php
+++ trunk/XML/GRDDL.php
@@ -1,15 +1,29 @@
 <?php
-class XML_GRDDL {
-    const NS = "http://www.w3.org/2003/g/data-view#";
+class XML_GRDDL
+{
+    const NS       = "http://www.w3.org/2003/g/data-view#";
     const XHTML_NS = 'http://www.w3.org/1999/xhtml';
 
-    public static function factory($driver = 'xsl', $options = array('documentTransformations' => true, 'htmlTransformations' => true)) {
+    /**
+     * Instantiate a new instance of a GRDDL driver.
+     *
+     * @param string  $driver  Name of driver. Default is 'xsl'.
+     * @param mixed[] $options An array of options, refer to individual drivers
+     *                         document for more information.
+     *
+     * @return  XML_GRDDL_Driver
+     */
+    public static function factory($driver = 'xsl',
+                                   $options = array('documentTransformations' => true,
+                                                    'htmlProfileTransformations' => true,
+                                                    'namespaceTransformations' => true))
+    {
         $class = 'XML_GRDDL_Driver_' . $driver;
 
         $path = dirname(__FILE__) . '/GRDDL/Driver/' . $driver . '.php';
 
         if (file_exists($path)) {
-            require_once $path;
+            include_once $path;
         }
 
         if (!class_exists($class)) {
